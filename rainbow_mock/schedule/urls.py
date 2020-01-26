7a4dda04cfd9645a301f8a6a728dc8970919d561
tfukami <f.t.21.8.3891@gmail.com>
2020-01-26 12:21:58 +0900

    add email test script

--- rainbow_mock/schedule/urls.py
+++ rainbow_mock/schedule/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
     path('setting-schedule/', views.add, name="schedule_setting"),
+    path('mail-test/', views.mail_test, name="mail_test"),
     path('private-schedule/', views.ScheduleView.as_view(), name="schedule_index"),
 ]
--- rainbow_mock/schedule/views.py
+++ rainbow_mock/schedule/views.py
@@ -94,3 +94,21 @@ class ScheduleView(LoginRequiredMixin, generic.ListView):
         )'''
         return context
 
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+def mail_test(request):
+    """題名"""
+    subject = "題名"
+    """本文"""
+    message = "本文です\nこんにちは。メールを送信しました"
+    """送信元メールアドレス"""
+    from_email = "information@myproject"
+    """宛先メールアドレス"""
+    recipient_list = [
+        ""
+    ]
+
+    send_mail(subject, message, from_email, recipient_list)
+    return HttpResponse('<h1>email send complete.</h1>')
